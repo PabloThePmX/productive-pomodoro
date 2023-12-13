9b746affc34badb2c877e93aae2208c5fe297a62
Pablo <pablosarturi1@gmail.com>
2023-12-13 00:37:50 -0300

    creating timer and notes webpages

---
+++ Controllers/PomodoroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivePomodoro.Controllers
+{
+    public class PomodoroController : Controller
+    {
+        public IActionResult Timer()
+        {
+            return View();
+        }
+
+        public IActionResult Notes()
+        {
+            return View();
+        }
+    }
+}
